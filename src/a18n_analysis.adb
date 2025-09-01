9fde5e73a4e78b49c40b1468a9080347d0350078
Jesper Quorning <jesper.quorning@gmail.com>
2025-09-01 23:40:41 +0200

    rem and mod are not unary

--- src/a18n_analysis.adb
+++ src/a18n_analysis.adb
@@ -66,26 +66,62 @@ package body A18n_Analysis is
 --         Put_Line ("  Direct: " & Name.P_Is_Direct_Call'Image);
 --         Put_Line ("  Access: " & Name.P_Is_Access_Call'Image);
 
-         if Name.P_Is_Access_Call then
-            return;
-         end if;
+--         if Name.P_Is_Access_Call then
+--            return;
+--         end if;
 
          if Debug then
-            Put_Line ("  Is_Dot  : " & Name.P_Is_Dot_Call (False)'Image);
+--            Put_Line ("  Is_Dot  : " & Name.P_Is_Dot_Call (False)'Image);
             Put_Line ("  Defining: " & Name.P_Is_Defining'Image);
-            Put_Line ("  Direct  : " & Name.P_Is_Direct_Call'Image);
-            Put_Line ("  Call    : " & Name.P_Is_Call'Image);
+--            Put_Line ("  Direct  : " & Name.P_Is_Direct_Call'Image);
+--            Put_Line ("  Call    : " & Name.P_Is_Call'Image);
             Put_Line ("  Operator: " & Name.P_Is_Operator_Name'Image);
          end if;
 
-         if Name.P_Is_Direct_Call then
+--         if Name.P_Is_Direct_Call then
             --  Is a procedure
-            return;
-         end if;
+--            return;
+--         end if;
 
          if Name.P_Is_Operator_Name then
             --  "-" () type of call
             Put_Line ("    !! ""-"" () type of call");
+            declare
+               First  : constant A.Ada_Node'Class := Node.First_Child;
+               Last   : constant A.Ada_Node'Class := Node.Last_Child;
+               Assoc  : constant A.Assoc_List     := Last.As_Assoc_List;
+               Params : constant A.Param_Actual_Array
+                  := Assoc.P_Zip_With_Params (Imprecise_Fallback => True);
+               Count  : Positive := 1;
+            begin
+               if Debug then
+                  Put_Line ("    Child F: " & First.Kind'Image);
+                  Put_Line ("    Child L: " & Last.Kind'Image);
+                  Put_Line ("    Text F : " & Text.Image (First.Text));
+               end if;
+
+               for Par of Params loop
+                  declare
+                     Defining   : constant A.Defining_Name'Class
+                        := A.Param (Par);
+                     Param_Name : constant String := Text.Image (Defining.F_Name.Text);
+--                     Basic_Decl : constant A.Basic_Decl := Defining.P_Basic_Decl;
+
+--                     Type_Name  : constant String
+--                        := Text.Image (Basic_Decl.P_Fully_Qualified_Name);
+
+--                     Base_Type  : constant A.Base_Type_Decl
+--                        := A.Base_Type_Decl (Basic_Decl); --  .As_Base_Type_Decl;
+                  begin
+                     Put ("    " & Count'Image & "=> ");
+                     Put (Param_Name);
+--                     Put (" :: " & Type_Name);
+                     New_Line;
+                     Count := Count + 1;
+                  end;
+               end loop;
+            end;
+
          else
             --  Gettext () type of call
             Put_Line ("    !! gettext () type of call");
@@ -204,9 +240,7 @@ package body A18n_Analysis is
       return     In_List (Ada_Op_Minus, "-")
          or else In_List (Ada_Op_Plus,  "+")
          or else In_List (Ada_Op_Not,   "not")
-         or else In_List (Ada_Op_Abs,   "abs")
-         or else In_List (Ada_Op_Mod,   "mod")
-         or else In_List (Ada_Op_Rem,   "ram");
+         or else In_List (Ada_Op_Abs,   "abs");
    end Operator_In;
 
 end A18n_Analysis;
